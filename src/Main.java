ed4262be89899e1a3ddb8007aab6d9c7f9a4842a
原田直幸 <katotakuto@teamB-MacBook.local>
2015-03-19 11:33:58 +0900

    ファイル読み込み処理実装

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 
         ReadRecipe readRecipe = new ReadRecipe();
 
+	String fileName = "../resource/file/recipe-data.txt";
+	String recipe = "";
 
-        System.out.println("オムライス");
+	try{
+	BufferedReader bufferedReader = readRecipe.readFile(fileName);
+
+	recipe = readRecipe.readLine(bufferedReader);
+	}
+	catch (IOException e) {
+	}
+
+        System.out.println(recipe);
     }
 }
